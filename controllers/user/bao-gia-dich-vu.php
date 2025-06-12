6a8a6c2d4a7b63813db34f4995e604fd0c379f09
subway90 <nguyenhieu3105@gmail.com>
2025-06-12 12:06:29 +0700

    update case bao-gia-dich-vu : handle step 1 when load page

--- controllers/user/bao-gia-dich-vu.php
+++ controllers/user/bao-gia-dich-vu.php
@@ -2,7 +2,7 @@
 
 
 # [RENDER]
-if(get_action_uri(1) == 'step-1') view('user','Chọn địa điểm','get-pricing-serivce-step-1',null);
+if(!get_action_uri(1) || get_action_uri(1) == 'step-1') view('user','Chọn địa điểm','get-pricing-serivce-step-1',null);
 if(get_action_uri(1) == 'step-2') view('user','Chọn dịch vụ','get-pricing-serivce-step-2',null);
 if(get_action_uri(1) == 'step-3') view('user','Chọn ngày giờ, số lượng','get-pricing-serivce-step-3',null);
 if(get_action_uri(1) == 'step-4') view('user','Thông tin & liên hệ','get-pricing-serivce-step-4',null);
